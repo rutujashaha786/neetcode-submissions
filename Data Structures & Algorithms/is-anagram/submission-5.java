f3f59cdd78afccb64d5e0bb6db9857ab82be4f6c
Rutuja Shaha <rutujashaha786@gmail.com>
2026-08-26 10:43:27 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        //Solution-4 : Fixed size array arr[26] as hash table
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        int[] arr = new int[26];
+
+        for(int i = 0; i < s.length(); i++){
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+
+            arr[ch1 - 'a']++;
+            arr[ch2 - 'a']--;
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
