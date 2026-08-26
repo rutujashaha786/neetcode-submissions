3a6df8c090c73975783cc61d341ef9a220e9e6ea
Rutuja Shaha <rutujashaha786@gmail.com>
2026-08-26 10:05:11 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        //Solution-3 : Create 2 freq HashMaps & compare both maps
+        if(s.length() != t.length()){
+            return false;
+        }
+        
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+            map1.put(ch1, map1.getOrDefault(ch1, 0) + 1);
+            map2.put(ch2, map2.getOrDefault(ch2, 0) + 1);
+        }
+
+        return map1.equals(map2);
+    }
+}
