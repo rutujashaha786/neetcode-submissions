7ab284ed52a548e6742bce2f27321ecbd5ff8f06
Rutuja Shaha <rutujashaha786@gmail.com>
2026-08-30 20:03:37 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+
+            if(Character.isLetterOrDigit(ch)){
+                sb.append(Character.toLowerCase(ch));
+            }
+        }
+
+        return sb.toString().equals(sb.reverse().toString());
+
+    }
+}
