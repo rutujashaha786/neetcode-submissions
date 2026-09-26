a23d227d2c37aff0539fc10471d3be9a7de29910
Rutuja Shaha <rutujashaha786@gmail.com>
2026-09-26 15:57:17 +0530

    Add: minimum-stack - submission-8

---
+++ Data Structures & Algorithms/minimum-stack/submission-8.java
@@ -0,0 +1,44 @@
+class MinStack {
+    Stack<Long> st;
+    long min;
+
+    public MinStack() {
+        st = new Stack<>();
+    }
+    
+    public void push(int val) {
+        if(st.isEmpty()){
+            st.push((long) val);
+            min = val;
+            return;
+        }
+        if(val > min){
+            st.push((long) val);
+        }
+        else{
+            st.push((long) val + (long)val - min);
+            min = val;
+        }
+    }
+    
+    public void pop() {
+        if(st.peek() < min){
+            //revert point for min
+            min = 2 * min - st.peek();
+        }
+        st.pop();
+    }
+    
+    public int top() {
+        if(st.peek() < min){
+            return (int)min;
+        }
+        else{
+            return (int)(long) st.peek();
+        }
+    }
+    
+    public int getMin() {
+        return (int) min;
+    }
+}
